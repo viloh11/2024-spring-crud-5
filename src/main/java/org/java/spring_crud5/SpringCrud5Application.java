9f5273fecd71261a7d25e3f05175a56af8a74650
Vincenzo <vincenzolong28@gmail.com>
2024-07-11 12:07:39 +0200

    First commit

---
+++ src/main/java/org/java/spring_crud5/SpringCrud5Application.java
@@ -0,0 +1,13 @@
+package org.java.spring_crud5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCrud5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCrud5Application.class, args);
+	}
+
+}
